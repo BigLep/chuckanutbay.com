eb0f0a10c731091c54b5a9ca060ec3864c9c5ca5
Steve Loeppky <stvn@loeppky.com>
2009-08-29 14:24:07 -0600

    setupDevo.sh prints out better messaging and creates necessary symlinks.

--- install/setupDevo.sh
+++ install/setupDevo.sh
@@ -3,9 +3,20 @@
 prodDir=$1
 devoDir=$prodDir/DEVO
 
-mkdir -p \
-	$devoDir/Applications/NutritionLabelImporter/toProcess \
-	$devoDir/Applications/QuickBooksItemsImporterImporter/toProcess \
-	$devoDir/dynamicPages/common
-	
+echo "Creating directories:"
+for dir in "Applications/NutritionLabelImporter/toProcess" "Applications/QuickBooksItemsImporterImporter/toProcess" "dynamicPages/common"; do
+	echo "$dir";
+	mkdir -p $devoDir/${dir}
+done
+echo
+
+echo "Creating symlinks:"
+for file in Images; do
+	echo "$file";
+	ln -s ${prodDir}/${file} ${devoDir}/${file} 
+done
+echo
+
+echo "Copying database connection info:"
 rsync -av $prodDir/dynamicPages/common/databaseConnection.php $devoDir/dynamicPages/common/
+echo
