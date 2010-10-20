4078c1df7314124dcedda030bec0f6da74f0f742
Steve Loeppky <stvn@loeppky.com>
2010-10-19 22:36:17 -0700

    Upgraded to use Ext 3.3 instead of 3.0.

--- dynamicPages/common/extJsIncludes.php
+++ dynamicPages/common/extJsIncludes.php
@@ -5,9 +5,9 @@
 		$extRoot = "http://extjs.cachefly.net";
 	}
 ?>
-		<link rel="stylesheet" type="text/css" href="<?php echo($extRoot); ?>/ext-3.0.0/resources/css/ext-all.css"/>
-    	<script type="text/javascript" src="<?php echo($extRoot); ?>/ext-3.0.0/adapter/ext/ext-base.js"></script>
-    	<script type="text/javascript" src="<?php echo($extRoot); ?>/ext-3.0.0/ext-all-debug.js"></script>
+		<link rel="stylesheet" type="text/css" href="<?php echo($extRoot); ?>/ext-3.3.0/resources/css/ext-all.css"/>
+    	<script type="text/javascript" src="<?php echo($extRoot); ?>/ext-3.3.0/adapter/ext/ext-base.js"></script>
+    	<script type="text/javascript" src="<?php echo($extRoot); ?>/ext-3.3.0/ext-all-debug.js"></script>
 		<script type="text/javascript">
-			Ext.BLANK_IMAGE_URL = '<?php echo($extRoot); ?>/ext-3.0.0/resources/images/default/s.gif';
+			Ext.BLANK_IMAGE_URL = '<?php echo($extRoot); ?>/ext-3.3.0/resources/images/default/s.gif';
 		</script>
